fe3632825f3e745a8fb643567d31027ebf5a0cb5
Lukas Danin <lukas.danin@gmail.com>
2020-10-29 23:10:11 -0300

    Improves Contact interfaces

--- src/contact.ts
+++ src/contact.ts
@@ -1,17 +1,28 @@
 export type CollectContactStatus = 'invalid' | 'success' | 'error'
 
+export type Projects =
+  | 'virtual-coffee'
+  | 'social-pull'
+  | 'globalunderdog'
+  | 'insurance'
+
 export interface Contact {
   /** Timestamp in ms */
   created?: number
   id?: string
   name?: string
   email: string
-  project: string
+  project: Projects
   linkedinUrl?: string
   googleTrackingId?: string
 }
 
 export interface ContactVirtualCoffee extends Contact {
+  project: 'virtual-coffee'
   asMentee: boolean
   asMentor: boolean
+}
+
+export interface ContactSocialPull extends Contact {
+  project: 'social-pull'
 }
