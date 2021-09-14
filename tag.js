4c2d538c156d14bf5aebbde33e58e7f19b266065
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2021-09-14 11:36:53 -0400

    Simplify package.json

---
+++ tag.js
@@ -0,0 +1,15 @@
+const { exec } = require('child_process')
+const { version } = require('./package.json')
+
+const commands = [
+  `git tag -a 'v${version}' -m 'v${version}'`,
+  `git push origin v${version}`,
+]
+
+const command = commands.join(' && ')
+
+if (process.argv.includes('--dryRun')) {
+  console.log(command)
+} else {
+  exec(command, () => console.log(`Added and pushed tag v${version}`))
+}
