3077d3b29f3dd15bf039b3b5f2dceee45227db5b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2021-09-15 11:49:49 -0400

    Add processEnvOrThrow

--- src/util.ts
+++ src/util.ts
@@ -1,2 +1,8 @@
 export const wait = (ms: number): Promise<void> =>
   new Promise((r) => setTimeout(r, ms))
+
+export const processEnvOrThrow = (key: string): string => {
+  const val = process.env[key]
+  if (!val) throw new Error(`Need to set environment variable ${key}`)
+  return val
+}
